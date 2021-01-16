a3de9eb6c2fbaaa12906824a1227c7daf25fe312
Igor Napierala <igor.napierala@cognifide.com>
2021-01-16 09:03:39 +0100

    server on http

---
+++ index_basic.js
@@ -0,0 +1,47 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+
+  if (req.url === "/about") {
+    fs.readFile(
+      path.join(__dirname, "public", "about.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+
+  if (req.url === "/api/users") {
+    const users = [
+      {
+        name: "John Doe",
+        age: 20,
+      },
+      {
+        name: "Jane Doe",
+        age: 30,
+      },
+    ];
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
