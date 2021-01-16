7f03f1dbb1c85abc5b63cb12991ec7c7ab8eb939
Igor Napierala <igor.napierala@cognifide.com>
2021-01-16 08:25:21 +0100

    uncommented some methods

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,36 +2,36 @@ const fs = require("fs");
 const path = require("path");
 
 // Create folder
-// fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
-//   if (err) throw err;
-//   console.log("Folder created...");
-// });
+fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
+  if (err) throw err;
+  console.log("Folder created...");
+});
 
 // Create and write to file
-// fs.writeFile(
-//   path.join(__dirname, "/test", "hello.txt"),
-//   "Hello World!",
-//   (err) => {
-//     if (err) throw err;
-//     console.log("File written to...");
+fs.writeFile(
+  path.join(__dirname, "/test", "hello.txt"),
+  "Hello World!",
+  (err) => {
+    if (err) throw err;
+    console.log("File written to...");
 
-//     // File append
-//     fs.appendFile(
-//       path.join(__dirname, "/test", "hello.txt"),
-//       " I love Node.js!",
-//       (err) => {
-//         if (err) throw err;
-//         console.log("File written to...");
-//       }
-//     );
-//   }
-// );
+    // File append
+    fs.appendFile(
+      path.join(__dirname, "/test", "hello.txt"),
+      " I love Node.js!",
+      (err) => {
+        if (err) throw err;
+        console.log("File written to...");
+      }
+    );
+  }
+);
 
 // Read file
-// fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf8", (err, data) => {
-//   if (err) throw err;
-//   console.log(data);
-// });
+fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
 
 // Rename file
 fs.rename(
